fab1e64893ef8c45acc218b7054da6f6de1beb09
Bhashana Sadanjana <bhashana1999@gmail.com>
2022-06-25 19:53:52 +0530

    Product_Rewamp

--- src/app/component/authentication/authentication.module.ts
+++ src/app/component/authentication/authentication.module.ts
@@ -3,15 +3,32 @@ import { CommonModule } from '@angular/common';
 
 import { AuthenticationRoutingModule } from './authentication-routing.module';
 import { AuthenticationComponent } from './authentication.component';
+import { LoginComponent } from './component/login/login.component';
+import { PasswordResetComponent } from './component/password-reset/password-reset.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {ReactiveFormsModule} from "@angular/forms";
+
 
 
 @NgModule({
   declarations: [
-    AuthenticationComponent
+    AuthenticationComponent,
+    LoginComponent,
+    PasswordResetComponent
   ],
   imports: [
     CommonModule,
-    AuthenticationRoutingModule
+    AuthenticationRoutingModule,
+    FlexLayoutModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule,
+    ReactiveFormsModule
   ]
 })
 export class AuthenticationModule { }
